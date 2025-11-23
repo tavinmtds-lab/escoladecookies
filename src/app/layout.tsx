513cd891f6368ef677fc0a59e1e8fb9b7ce77d1f
Guilherme MA <Guitec2016@gmail.com>
2025-11-23 02:59:54 +0000

    por favor coloque meus pixels da utmify na pagina:
    <script>
    window.pix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display, Poppins, Montserrat } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Escola de Cookies',
@@ -39,6 +40,23 @@ export default function RootLayout({
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <head>
+        <Script id="utmify-pixel-config" strategy="afterInteractive">
+          {`
+            window.pixelId = "69227825044f8767fa1a8fc0";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></Script>
       </head>
       <body className={cn("font-body antialiased", poppins.variable, playfairDisplay.variable, montserrat.variable)}>
         {children}
